5fbdd827a600b09af2566affa67d410a573d805c
Jaywant Lugun <jaywantlugun2000@gmail.com>
2025-08-17 19:41:24 +0530

    test1

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit in screen.</Text>
+      <Text>Edit app/index.tsx to edit in the screen.</Text>
     </View>
   );
 }
